0b9b1004584c1bbb29ac078f0efc934bca4d4df1
merinov aleks <merinovaleks1922@gmail.com>
2022-06-02 18:36:27 +0300

    popup login

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -47,3 +47,79 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 
 });
+
+
+
+
+  // Open Popup
+  const body = document.querySelector('body');
+  const openPopupButtons = document.querySelectorAll('[data-open-popup]');
+  if (openPopupButtons) {
+      openPopupButtons.forEach((openPopupButton) => {
+          openPopupButton.addEventListener('click', (event) => {
+
+              event.preventDefault();
+              const popupName = event.target.dataset.openPopup;
+              const popups = document.querySelectorAll('.popup');
+              if (popups) {
+                  popups.forEach((popup) => {
+                      if (popup.dataset.popupName == popupName) {
+                          popup.classList.add('popup--active');
+                          body.classList.add('lock');
+                      } else {
+                          body.classList.remove('lock');
+                      }
+                  });
+              }
+          });
+      });
+ 
+  }
+
+  // Close All Popups Function
+
+   const closeAllPopups = () => {
+      const popups = document.querySelectorAll('.popup');
+      if (popups) {
+         popups.forEach((popup) => {
+            popup.classList.remove('popup--active');
+            body.classList.remove('lock');
+         });
+      }
+   };
+
+
+  const closePopupButtons = document.querySelectorAll('[data-close-popup]');
+   if (closePopupButtons) {
+      closePopupButtons.forEach((closePopupButton) => {
+         closePopupButton.addEventListener('click', (event) => {
+               event.preventDefault();
+               closeAllPopups();
+         });
+      });
+   }
+
+// Close Popup After Outside Click
+
+
+
+const popups = document.querySelectorAll('.popup');
+if (popups) {
+    popups.forEach((popup) => {
+        popup.addEventListener('mousedown', (event) => {
+            if (!event.target.closest('.popup__window')) {
+               event.preventDefault();
+               closeAllPopups();
+            }
+        });
+    });
+}
+
+// Close Popups After "Escape" Key Press
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'Escape') {
+         event.preventDefault();
+         closeAllPopups();
+    }
+});
